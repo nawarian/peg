a38491bed94211b56fb90207bb6dc890ee76260c
Níckolas Da Silva <nickolas.silva@researchgate.net>
2020-12-23 14:43:09 +0100

    Implement Rule\Any

--- src/Rule/Any.php
+++ src/Rule/Any.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 namespace Nawarian\PEG\Rule;
 
+use Nawarian\PEG\Span;
+
 final class Any implements Rule
 {
     public function match(string $text)
     {
-        throw new \Exception('Not implemented.');
+        if (mb_strlen($text) > 0) {
+            return new Span(mb_substr($text, 0, 1));
+        }
+
+        return false;
     }
 }
---
+++ tests/Rule/AnyTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use Nawarian\PEG\Rule\Any;
+use Nawarian\PEG\Rule\RuleFactory;
+use Nawarian\PEG\Span;
+
+it('Should match all', function (Any $any, string $char, string $expectedChar) {
+    expect($any->match($char))->toEqual(new Span($expectedChar));
+})->with([
+    [RuleFactory::any(), 'N', 'N'],
+    [RuleFactory::any(), '0', '0'],
+    [RuleFactory::any(), '-', '-'],
+    [RuleFactory::any(), '.', '.'],
+    [RuleFactory::any(), '$', '$'],
+    [RuleFactory::any(), PHP_EOL, PHP_EOL],
+]);
+
+it('Should fail if matching string is empty', function (Any $any) {
+    expect($any->match(''))->toBeFalse();
+})->with([RuleFactory::any()]);
